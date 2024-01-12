c059544da23948ebeb1ad7c5abccd1537fb9962f
yash131120 <139432375+yash131120@users.noreply.github.com>
2024-01-12 18:23:13 +0530

    for 2k & 4k resolutions

--- assets/js/main.js
+++ assets/js/main.js
@@ -118,11 +118,14 @@ const sr = ScrollReveal({
   // reset: true
 })
 
-sr.reveal(`.home__title`)
-sr.reveal(`.home__description`, { delay: 500 })
+sr.reveal(`.home__title, .popular__container, .subscribe__container, .footer__container`)
+sr.reveal(`.home__description, .footer__info`, { delay: 500 })
 sr.reveal(`.home__search`, { delay: 600 })
 sr.reveal(`.home__value`, { delay: 700 })
 sr.reveal(`.home__images`, { delay: 800, origin: 'bottom' })
-sr.reveal(`.home__value`, { delay: 700 })
+sr.reveal(`.logos__img`, { interval: 100 })
+sr.reveal(`.value__images, .contact__content`, { origin: 'left' })
+sr.reveal(`.value__content, .contact__images`, { origin: 'right' })
+
 
 
